29f9bed129a8b7e637c2e8399ea1dcf58a433359
Ivan Terekhin <i.terhin@gmail.com>
2020-10-09 12:46:27 +0300

    Widget instead of icon

--- lib/modals.dart
+++ lib/modals.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 
 class FocusedMenuItem {
-    Color backgroundColor;
-    Widget title;
-    Icon trailingIcon;
-    Function onPressed;
+  Color backgroundColor;
+  Widget title;
+  Widget trailingIcon;
+  Function onPressed;
 
-    FocusedMenuItem({this.backgroundColor,@required this.title, this.trailingIcon,@required this.onPressed});
+  FocusedMenuItem(
+      {this.backgroundColor,
+      @required this.title,
+      this.trailingIcon,
+      @required this.onPressed});
 }
