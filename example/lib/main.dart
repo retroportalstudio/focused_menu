fad2ad2e4afe791ca7031cb6e333e7c582d9b525
Henrique Hadermeck <henriquezanferrari@gmail.com>
2023-01-08 11:54:29 +0100

    doc: adding more examples

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
-import 'package:example/ScreenTwo.dart';
+import 'package:example/example1.dart';
+import 'package:example/example2.dart';
 import 'package:flutter/material.dart';
-import 'package:focused_menu/focused_menu.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,149 +14,58 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Focused Menu',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          titleTextStyle: TextStyle(color: Colors.black, fontSize: 20),
+          elevation: 0,
+          centerTitle: true,
+          iconTheme: IconThemeData(color: Colors.black),
+          systemOverlayStyle: SystemUiOverlayStyle(
+            statusBarIconBrightness: Brightness.dark,
+            statusBarBrightness: Brightness.light,
+          ),
+        ),
       ),
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  FocusedMenuHolderController _avatarController = FocusedMenuHolderController();
+class HomePage extends StatelessWidget {
+  const HomePage({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(15.0),
+      appBar: AppBar(title: Text('Focused Menu')),
+      body: Center(
         child: Column(
-          children: <Widget>[
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Music Albums",
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                Expanded(child: Center()),
-                IconButton(icon: Icon(Icons.shopping_cart), onPressed: () {}),
-                FocusedMenuHolder(
-                  controller: _avatarController,
-                  onPressed: () {},
-                  menuItems: <FocusedMenuItem>[
-                    FocusedMenuItem(
-                      title: Text("This is a button"),
-                      trailing: Icon(Icons.open_in_new),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ScreenTwo(),
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                  child: CircleAvatar(
-                    child: Image.asset("assets/images/dp_default.png"),
-                  ),
-                )
-              ],
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => Example1()),
+                );
+              },
+              child: Text('OnTap Example'),
             ),
             ElevatedButton(
-              onPressed: () => _avatarController.open(),
-              child: Text('Open avatar menu'),
-            ),
-            SizedBox(height: 10),
-            TextField(
-              decoration: InputDecoration(
-                border: InputBorder.none,
-                hintText: "Look for your Interest!",
-                fillColor: Colors.grey.shade200,
-                filled: true,
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Expanded(
-              child: GridView(
-                physics: BouncingScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2),
-                children: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-                    .map((e) => FocusedMenuHolder(
-                          menuWidth: MediaQuery.of(context).size.width * 0.50,
-                          blurSize: 5.0,
-                          menuItemExtent: 45,
-                          menuBoxDecoration: BoxDecoration(
-                              color: Colors.grey,
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(15.0))),
-                          duration: Duration(milliseconds: 100),
-                          animateMenuItems: true,
-                          blurBackgroundColor: Colors.black54,
-                          bottomOffsetHeight: 100,
-                          openWithTap: true,
-                          menuItems: <FocusedMenuItem>[
-                            FocusedMenuItem(
-                                title: Text("Open"),
-                                trailing: Icon(Icons.open_in_new),
-                                onPressed: () {
-                                  Navigator.push(
-                                      context,
-                                      MaterialPageRoute(
-                                          builder: (context) => ScreenTwo()));
-                                }),
-                            FocusedMenuItem(
-                                title: Text("Share"),
-                                trailing: Icon(Icons.share),
-                                onPressed: () {}),
-                            FocusedMenuItem(
-                                title: Text("Favorite"),
-                                trailing: Icon(Icons.favorite_border),
-                                onPressed: () {}),
-                            FocusedMenuItem(
-                                title: Text(
-                                  "Delete",
-                                  style: TextStyle(color: Colors.redAccent),
-                                ),
-                                trailing: Icon(
-                                  Icons.delete,
-                                  color: Colors.redAccent,
-                                ),
-                                onPressed: () {}),
-                          ],
-                          onPressed: () {},
-                          child: Card(
-                            child: Column(
-                              children: <Widget>[
-                                Image.asset("assets/images/image_$e.jpg"),
-                              ],
-                            ),
-                          ),
-                        ))
-                    .toList(),
-              ),
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => Example2()),
+                );
+              },
+              child: Text('Controller Example'),
             ),
           ],
         ),
-      )),
-      bottomNavigationBar: BottomNavigationBar(items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Home"),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Menu 2"),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Menu 3"),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Menu 4"),
-        BottomNavigationBarItem(icon: Icon(Icons.add), label: "Menu 5"),
-      ]),
+      ),
     );
   }
 }
